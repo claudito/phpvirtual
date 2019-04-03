8ced9abe753fb86a8c407b12bd91208e7bf99ce6
claudito <1107000044@upci.edu.pe>
2019-04-02 20:12:25 -0500

    datatable server side

---
+++ semana04/index.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+<!-- Css -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+
+<!-- Css Datatable -->
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
+
+<!-- Css FontAwesome -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+	<title>Empleados</title>
+</head>
+<body>
+	
+<!-- Contenedor -->
+<div class="container-fluid">
+
+<!-- Fila  1 -->
+<div class="row">
+
+<!-- Columna 12 -->
+<div class="col-md-12">
+
+
+<button class="btn btn-primary btn-agregar" ><i class="fa fa-plus"></i> Agregar</button>
+
+<hr>
+	
+<div class="table-responsive">
+
+<table id="consulta" class="table">
+	
+<thead>
+<tr>
+<th>Id</th>
+<th>Nombres</th>
+<th>Apellidos</th>
+</tr>	
+
+</thead>
+
+
+
+
+</table>
+
+</div>
+
+
+
+</div>
+
+
+
+</div>
+
+</div>
+
+
+<!-- Modal Registro(Agregar / Actualizar) -->
+<form id="registro" autocomplete="off">
+	
+<div class="modal fade" id="modal-registro" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      
+     
+<div class="form-group">
+<label>Cargo</label>
+<select name="cargo"  class="cargo form-control" required></select>
+</div>
+
+
+<div class="form-group">
+<label>Sede</label>
+<select name="sede" class="sede form-control" required></select>
+</div>
+
+
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+
+        <button type="button" class="btn btn-primary btn-submit">Save changes</button>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+</form>
+
+
+
+
+
+<!-- JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+<!-- JS Datatable -->
+
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+
+<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+
+<script>
+	
+$(document).ready(function (){
+
+$('#consulta').DataTable({
+
+"deferRender":true,
+"iDisplayLength": 25,
+ "bProcessing": true,
+"sAjaxSource": "source.php?op=1",
+"aoColumns":[
+
+{ mData:"id"},
+{ mData:"nombres"},
+{ mData:"apellidos"}
+
+
+],
+"language":{
+
+"url":"https://cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json"
+
+
+}
+
+
+});
+
+
+});
+
+
+//Cargar Modal Agregar
+$(document).on('click','.btn-agregar',function(){
+
+//Lista de Cargos
+cargo  = '<option value="">[Seleccionar]</option>';
+
+url    = 'source.php?op=4';
+
+$.getJSON(url,{},function(data){
+
+data.forEach(function (row){
+
+cargo += '<option value="'+row.id+'">'+row.nombre+'</option>';
+
+$('.cargo').html(cargo);
+
+
+});
+
+
+});
+
+//Sede
+
+sede = '<option value="">[Seleccionar]</option>';
+
+url  = 'source.php?op=3';
+
+$.getJSON(url,{},function(data){
+
+
+data.forEach(function (row){
+
+
+sede += '<option value="'+row.id+'">'+row.nombre+'</option>';
+
+$('.sede').html(sede);
+
+});
+
+});
+
+
+
+$('.modal-title').html('Agregar');
+$('.btn-submit').html('Agregar');
+$('#modal-registro').modal('show');
+
+
+});
+
+
+//Cargar Modal Actualizar
+$(document).on('click','.btn-edit',function(){
+
+
+$('.modal-title').html('Actualizar');
+$('.btn-submit').html('Actualizar');
+$('#modal-registro').modal('show');
+
+
+});
+
+
+
+</script>
+
+</body>
+</html>
---
+++ semana04/source.php
@@ -0,0 +1,112 @@
+<?php 
+
+include'modelo/Conexion.php';
+
+$conexion = new Conexion();
+$conexion = $conexion->get_conexion();
+
+$opcion   = $_REQUEST['op'];
+
+/*
+
+1 =  lista de empleados
+2 =  agregar y actualizar empleados
+3 =  lista de sedes
+4 =  lista de cargos
+5 =  eliminar / inactivar empleados 
+
+*/
+
+switch ($opcion) {
+	case 1:
+
+try {
+	
+$query     =  "SELECT * FROM empleado";
+$statement =  $conexion->prepare($query);
+$statement->execute();
+$result    =  $statement->fetchAll(PDO::FETCH_ASSOC);
+
+$json = [
+
+'sEcho'=>1,
+'iTotalRecords'=>count($result),
+'iTotalDisplayRecords'=>count($result),
+'aaData'=>$result
+
+];
+
+
+echo json_encode($json);
+
+
+
+} catch (Exception $e) {
+	
+echo "Error: ".$e->getMessage();
+
+}
+
+
+
+		break;
+	case 2:
+		# code...
+		break;
+
+	case 3:
+	 
+	try {
+		
+     $query     =  "SELECT * FROM sede";
+     $statement = $conexion->prepare($query);
+     $statement->execute();
+     $result    = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+     echo json_encode($result);
+
+	} catch (Exception $e) {
+		
+    echo "Error: ".$e->getMessage();
+
+	}
+
+
+
+
+		break;
+
+	case 4:
+	
+    	try {
+		
+     $query     =  "SELECT * FROM cargo";
+     $statement = $conexion->prepare($query);
+     $statement->execute();
+     $result    = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+     echo json_encode($result);
+
+	} catch (Exception $e) {
+		
+    echo "Error: ".$e->getMessage();
+
+	}
+
+
+
+		break;
+
+	case 5:
+		# code...
+		break;
+
+	
+	default:
+	echo "opción no disponible";
+		break;
+}
+
+
+
+ ?>
